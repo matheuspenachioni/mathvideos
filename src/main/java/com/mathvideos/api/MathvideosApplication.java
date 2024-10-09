8638b20913fc98ba45e3c84497a6d69358ec4507
matheuspenachioni <matheus.penachioni2001@gmail.com>
2024-10-09 13:37:19 -0300

    first commit

---
+++ src/main/java/com/mathvideos/api/MathvideosApplication.java
@@ -0,0 +1,13 @@
+package com.mathvideos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathvideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathvideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathvideos/api/MathvideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathvideos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathvideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
